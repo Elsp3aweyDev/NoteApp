c9a5df0a74d387fad252ff846b4f1245389d70cc
elsp3aweyDev <mohsen.elsp3awey@gmail.com>
2025-09-12 12:43:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:my_note/views/home_page.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  List todoList = [
+    ["learn flutter", false],
+    ["drink coffee", false],
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView.builder(
+          itemCount: 1,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                
+              ],
+            );
+          },
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              color: Colors.deepPurple,
+              child: Center(
+                child: Text(
+                  style: TextStyle(color: Colors.white),
+                  todoList[index][0],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Note app"),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.purple,
+    );
+  }
+}
